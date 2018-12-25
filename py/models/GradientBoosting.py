c37aafa773534c8526c0b9fc5b848381c64dca9f
takuto0831 <takut0.mirage@gmail.com>
2018-12-25 15:56:41 +0900

    バリデーションの主要部分と手法のアルゴリズム部分を分離

--- py/models/GradientBoosting.py
+++ py/models/GradientBoosting.py
@@ -6,39 +6,31 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error
 
 def Validation(k):
-    return StratifiedKFold(n_splits=k, shuffle=True, random_state=15)
-def Lightgbm_Regressor():
-  # parameters
-  param = {'num_leaves': 31,
-           'min_data_in_leaf': 30, 
-           'objective':'regression',
-           'max_depth': -1,
-           'learning_rate': 0.01,
-           "min_child_samples": 20,
-           "boosting": "gbdt",
-           "feature_fraction": 0.9,
-           "bagging_freq": 1,
-           "bagging_fraction": 0.9 ,
-           "bagging_seed": 11,
-           "metric": 'rmse',
-           "lambda_l1": 0.1,
-           "verbosity": -1,
-           "nthread": 4,
-           "random_state": 831}
-  # model         
-  oof_lgb = np.zeros(len(train))
-  predictions_lgb = np.zeros(len(test))
-  ## k-stratified k-Fold
-  folds = Validation(5)
-  for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):    
-    print('-'); print("Fold {}".format(fold_ + 1));
+    return StratifiedKFold(n_splits=k, shuffle=True, random_state=831)
+    
+def Regressors(algorithm,param_set,train,test,features,target,folds=5):
+    ## predict data box
+    validation_pred = np.zeros(train.shape[0])
+    test_pred = np.zeros(test.shape[0])
+    ## k-stratified k-Fold
+    folds = Validation(folds)
+    # 外れ値を考慮して, データを分割する
+    for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
+        print("fold n°{}".format(fold_+1))
+        # make model
+        val_idx, model = algorithm(train,trn_idx,val_idx,features,target,param_set)
+        # predicting validation data and predicting data
+        validation_pred[val_idx] = model.predict(train.iloc[val_idx][features], num_iteration=model.best_iteration)
+        test_pred += model.predict(test[features], num_iteration=model.best_iteration) / folds.n_splits
+    return validation_pred, test_pred, model
+    
+def Lightgbm_Regressor(train,trn_idx,val_idx,features,target,param_set):
+    # data set
     trn_data = lgb.Dataset(train.iloc[trn_idx][features], label=target.iloc[trn_idx])
     val_data = lgb.Dataset(train.iloc[val_idx][features], label=target.iloc[val_idx])
-
-    num_round = 10000
-    clf = lgb.train(param, trn_data, num_round, valid_sets = [trn_data, val_data], verbose_eval=100, early_stopping_rounds=200)
-    oof_lgb[val_idx] = clf.predict(train.iloc[val_idx][features], num_iteration=clf.best_iteration)
-    predictions_lgb += clf.predict(test[features], num_iteration=clf.best_iteration) / folds.n_splits
-  return oof_lgb, predictions_lgb
+    # model
+    model = lgb.train(param_set, trn_data, num_round=10000, valid_sets = [trn_data, val_data], 
+                      verbose_eval=100, early_stopping_rounds=200)
+    return val_idx, model
 
 
--- py/models/Regressor.py
+++ py/models/Regressor.py
@@ -6,89 +6,39 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error
 
-def Validation(k):
-  return StratifiedKFold(n_splits=k, shuffle=True, random_state=15)
-def Ridge_Regressor(train,test,features,target):
-  # model
-  ## predict data box
-  val_pred_ridge = np.zeros(train.shape[0])
-  test_pred_ridge = np.zeros(test.shape[0])
-  ## make test data
-  test_data = test.copy()
-  test_data.fillna((test_data.mean()), inplace=True)
-  test_data = test_data[features].values
-  ## k-stratified k-Fold
-  folds = Validation(5)
-  ## 実行
-  # 外れ値を考慮して, データを分割する
-  for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
-    print("fold n°{}".format(fold_+1))
-    trn_data, trn_y = train.iloc[trn_idx][features], target.iloc[trn_idx].values
-    val_data, val_y = train.iloc[val_idx][features], target.iloc[val_idx].values
-    # fill missing values
-    trn_data.fillna((trn_data.mean()), inplace=True); val_data.fillna((val_data.mean()), inplace=True);
-    # extract data
-    trn_data = trn_data.values; val_data = val_data.values;
-    # fitting model for train data
-    clf = Ridge(alpha=100); clf.fit(trn_data, trn_y);
-    # predicting validation data and predicting data
-    val_pred_ridge[val_idx] = clf.predict(val_data)
-    test_pred_ridge += clf.predict(test_data) / folds.n_splits
-  return val_pred_ridge, test_pred_ridge
-  
-def Lasso_Regressor(train,test,features,target):
-  # model
-  ## predict data box
-  val_pred_ridge = np.zeros(train.shape[0])
-  test_pred_ridge = np.zeros(test.shape[0])
-  ## make test data
-  test_data = test.copy()
-  test_data.fillna((test_data.mean()), inplace=True)
-  test_data = test_data[features].values
-  ## k-stratified k-Fold
-  folds = Validation(5)
-  ## 実行
-  # 外れ値を考慮して, データを分割する
-  for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
-    print("fold n°{}".format(fold_+1))
-    trn_data, trn_y = train.iloc[trn_idx][features], target.iloc[trn_idx].values
-    val_data, val_y = train.iloc[val_idx][features], target.iloc[val_idx].values
-    # fill missing values
-    trn_data.fillna((trn_data.mean()), inplace=True); val_data.fillna((val_data.mean()), inplace=True);
-    # extract data
-    trn_data = trn_data.values; val_data = val_data.values;
-    # fitting model for train data
-    clf = Lasso(alpha=100); clf.fit(trn_data, trn_y);
-    # predicting validation data and predicting data
-    val_pred_ridge[val_idx] = clf.predict(val_data)
-    test_pred_ridge += clf.predict(test_data) / folds.n_splits
-  return val_pred_ridge, test_pred_ridge
+### fitting model list ###
+# Ridde(alpha=100)
+# Lasso(alpha=100)
+# BaggingRegressor(DecisionTreeRegressor(), n_estimators=100, max_samples=0.3)
 
-def Bagging_Regressor(train,test,features,target):
-  # model
-  ## predict data box
-  val_pred_ridge = np.zeros(train.shape[0])
-  test_pred_ridge = np.zeros(test.shape[0])
-  ## make test data
-  test_data = test.copy()
-  test_data.fillna((test_data.mean()), inplace=True)
-  test_data = test_data[features].values
-  ## k-stratified k-Fold
-  folds = Validation(5)
-  ## 実行
-  # 外れ値を考慮して, データを分割する
-  for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
-    print("fold n°{}".format(fold_+1))
-    trn_data, trn_y = train.iloc[trn_idx][features], target.iloc[trn_idx].values
-    val_data, val_y = train.iloc[val_idx][features], target.iloc[val_idx].values
-    # fill missing values
-    trn_data.fillna((trn_data.mean()), inplace=True); val_data.fillna((val_data.mean()), inplace=True);
-    # extract data
-    trn_data = trn_data.values; val_data = val_data.values;
-    # fitting model for train data
-    reg = BaggingRegressor(DecisionTreeRegressor(), n_estimators=100, max_samples=0.3)
-    reg.fit(trn_data, trn_y)
-    # predicting validation data and predicting data
-    val_pred_ridge[val_idx] = reg.predict(val_data)
-    test_pred_ridge += reg.predict(test_data) / folds.n_splits
-  return val_pred_ridge, test_pred_ridge
+def Validation(k):
+    return StratifiedKFold(n_splits=k, shuffle=True, random_state=831)
+
+def Regressors(clf,train,test,features,target,folds=5):
+    ## predict data box
+    validation_pred = np.zeros(train.shape[0])
+    test_pred = np.zeros(test.shape[0])
+    ## make test data
+    test_data = test.copy()
+    test_data.fillna((test_data.mean()), inplace=True)
+    test_data = test_data[features].values
+    ## k-stratified k-Fold
+    folds = Validation(folds)
+    # 外れ値を考慮して, データを分割する
+    for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
+        print("fold n°{}".format(fold_+1))
+        trn_data, trn_y = train.iloc[trn_idx][features], target.iloc[trn_idx].values
+        val_data, val_y = train.iloc[val_idx][features], target.iloc[val_idx].values
+        # fill missing values
+        trn_data.fillna((trn_data.mean()), inplace=True);
+        val_data.fillna((val_data.mean()), inplace=True);
+        # extract data
+        trn_data = trn_data.values; val_data = val_data.values;
+        # fitting arvitrary model for train data
+        clf = Ridge(alpha=100); clf.fit(trn_data, trn_y);
+        # predicting validation data and predicting data
+        validation_pred[val_idx] = clf.predict(val_data)
+        test_pred += clf.predict(test_data) / folds.n_splits
+    return validation_pred, test_pred
+
+
