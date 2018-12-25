cf7d3d01ca720d987ba897a37679c26be97a8d83
takuto0831 <takut0.mirage@gmail.com>
2018-12-25 16:50:44 +0900

    add xgboost regressor

--- py/models/GradientBoosting.py
+++ py/models/GradientBoosting.py
@@ -18,19 +18,31 @@ def Regressors(algorithm,param_set,train,test,features,target,folds=5):
     for fold_, (trn_idx, val_idx) in enumerate(folds.split(train,train['outliers'].values)):
         print("fold n°{}".format(fold_+1))
         # make model
-        val_idx, model = algorithm(train,trn_idx,val_idx,features,target,param_set)
-        # predicting validation data and predicting data
-        validation_pred[val_idx] = model.predict(train.iloc[val_idx][features], num_iteration=model.best_iteration)
-        test_pred += model.predict(test[features], num_iteration=model.best_iteration) / folds.n_splits
+        validation_pred, test_pred, model = algorithm(train,trn_idx,val_idx,features,target,param_set)
     return validation_pred, test_pred, model
     
-def Lightgbm_Regressor(train,trn_idx,val_idx,features,target,param_set):
+def Lightgbm_Regressor(train,trn_idx,val_idx,features,target,param_set,validation_pred,test_pred):
     # data set
     trn_data = lgb.Dataset(train.iloc[trn_idx][features], label=target.iloc[trn_idx])
     val_data = lgb.Dataset(train.iloc[val_idx][features], label=target.iloc[val_idx])
     # model
     model = lgb.train(param_set, trn_data, num_boost_round=10000, valid_sets = [trn_data, val_data], 
                       verbose_eval=100, early_stopping_rounds=200)
-    return val_idx, model
+    # predicting validation data and predicting data
+    validation_pred[val_idx] = model.predict(train.iloc[val_idx][features], num_iteration=model.best_iteration)
+    test_pred += model.predict(test[features], num_iteration=model.best_iteration) / folds.n_splits
+    return validation_pred, test_pred, model
 
-
+def Xgboost_Regressor(train,trn_idx,val_idx,features,target,param_set,validation_pred,test_pred):
+    # data set
+    trn_data = xgb.DMatrix(data=train.iloc[trn_idx][features], label=target.iloc[trn_idx])
+    val_data = xgb.DMatrix(data=train.iloc[val_idx][features], label=target.iloc[val_idx])
+    # model
+    model = xgb.train(param_set, trn_data, watchlist = [(trn_data, 'train'), (val_data, 'valid')], 
+                      num_boost_round = 10000, early_stopping_rounds=200, verbose_eval= 100)
+    # predicting validation data and predicting data             
+    validation_pred[val_idx] = model.predict(xgb.DMatrix(train.iloc[val_idx][features]), 
+                                           ntree_limit = model.best_ntree_limit+50)
+    test_pred += model.predict(xgb.DMatrix(test[features]), 
+                               ntree_limit=model.best_ntree_limit+50) / folds.n_splits
+    return validation_pred, test_pred, model
--- py/models/__init__.py
+++ py/models/__init__.py
@@ -1,2 +1,3 @@
 from Regressor import Regressors
-from GradientBoosting import Regressors,Lightgbm_Regressor
+from GradientBoosting import Regressors,Lightgbm_Regressor,Xgboost_Regressor
+from Ensemble 
