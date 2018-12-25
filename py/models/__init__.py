538debc1f476584ef94727a0723e90451632e0a6
takuto0831 <takut0.mirage@gmail.com>
2018-12-25 15:57:18 +0900

    update

--- py/models/__init__.py
+++ py/models/__init__.py
@@ -1,2 +1,2 @@
-from Regressor import Ridge_Regressor, Lasso_Regressor, Bagging_Regressor
-from Boosting import Lightgbm_Regressor
+from Regressor import Regressors
+from GradientBoosting import Regressors,Lightgbm_Regressor
