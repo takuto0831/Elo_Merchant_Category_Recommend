98c691fd9ba8eeb1dbb749bec3edb3f84655d2a2
takuto0831 <takut0.mirage@gmail.com>
2019-01-02 00:46:28 +0900

    add gradient boosting visualization method

--- py/base.py
+++ py/base.py
@@ -2,7 +2,9 @@ import numpy as np # linear algebra
 import pandas as pd # data processing
 import feather # fast reading data
 from datetime import datetime
-import pickle
+import pickle,requests
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def read_data(train_name,test_name,features_name, home_path):
     #Loading Train and Test Data
@@ -23,9 +25,27 @@ def submit(predict,tech, home_path):
     submit_file = feather.read_dataframe(home_path + "/Desktop/Elo_kaggle/input/feather/sample_submission.feather")
     submit_file["target"] = predict
     # save for output/(technic name + datetime + .csv)
-    file_name = home_path + '/Desktop/Elo_kaggle/output/' + tech + datetime.now().strftime("%Y%m%d") + ".csv"
+    file_name = home_path + '/Desktop/Elo_kaggle/output/submit/' + tech + datetime.now().strftime("%Y%m%d") + ".csv"
     submit_file.to_csv(file_name, index=False)
 def open_parameter(file_name, home_path):
     f = open(home_path + '/Desktop/Elo_kaggle/input/parameters/' + file_name + '.txt', 'rb')
     list_ = pickle.load(f)
     return list_
+def line(text):
+    line_notify_token = '07tI1nvYaAtGaLdsCaxKZxkboOU0OsvLregXqodN2ZV' #先程発行したコードを貼ります
+    line_notify_api = 'https://notify-api.line.me/api/notify'
+    message = '\n' + text
+    #変数messageに文字列をいれて送信します トークン名の隣に文字が来てしまうので最初に改行しました
+    payload = {'message': message}
+    headers = {'Authorization': 'Bearer ' + line_notify_token}
+    line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+def display_importances(features,model,title,home_path,file_name = None):
+    # make data frame
+    importance_df = pd.DataFrame({'feature':features, 'importance':model.feature_importance()})
+    plt.figure(figsize=(14,25))
+    sns.barplot(x="importance",y="feature",
+                data=importance_df.sort_values(by="importance", ascending=False))
+    plt.title(title)
+    plt.tight_layout()
+    if file_name is not None: 
+        plt.savefig(home_path + '/Desktop/Elo_kaggle/output/image/' + file_name)
