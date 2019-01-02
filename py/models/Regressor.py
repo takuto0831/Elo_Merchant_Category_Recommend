0cc18c866522c34fd319ae8ca4391dc98bd3b188
takuto0831 <takut0.mirage@gmail.com>
2019-01-02 19:25:10 +0900

    update

--- py/models/Regressor.py
+++ py/models/Regressor.py
@@ -20,7 +20,8 @@ def Regressors(clf,train,test,features,target,folds=5):
     test_pred = np.zeros(test.shape[0])
     ## make test data
     test_data = test.copy()
-    test_data = test_data.replace([np.inf, -np.inf], np.nan).fillna((test_data.mean()), inplace=True)
+    test_data = test_data.replace([np.inf, -np.inf], np.nan) # inf 処理
+    test_data = test_data.fillna((test_data.mean()), inplace=True) # nan 処理
     test_data = test_data[features].values
     ## k-stratified k-Fold
     folds = Validation(folds)
@@ -29,9 +30,12 @@ def Regressors(clf,train,test,features,target,folds=5):
         print("fold n°{}".format(fold_+1))
         trn_data, trn_y = train.iloc[trn_idx][features], target.iloc[trn_idx].values
         val_data, val_y = train.iloc[val_idx][features], target.iloc[val_idx].values
+        # fill Inf values
+        trn_data = trn_data.replace([np.inf, -np.inf], np.nan)
+        val_data = val_data.replace([np.inf, -np.inf], np.nan)
         # fill missing values
-        trn_data = trn_data.replace([np.inf, -np.inf], np.nan).fillna((trn_data.mean()), inplace=True);
-        val_data = val_data.replace([np.inf, -np.inf], np.nan).fillna((val_data.mean()), inplace=True);
+        trn_data = trn_data.fillna((trn_data.mean()), inplace=True);
+        val_data = val_data.fillna((val_data.mean()), inplace=True);
         # extract data
         trn_data = trn_data.values; val_data = val_data.values;
         # fitting arvitrary model for train data
