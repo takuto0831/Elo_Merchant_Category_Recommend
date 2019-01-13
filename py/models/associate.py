51c841b0586e11df7de714ad2ca44b91bb1f2f93
takuto0831 <takut0.mirage@gmail.com>
2019-01-14 00:27:55 +0900

    remove

--- py/models/associate.py
+++
@@ -1,6 +0,0 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing 
-from sklearn.model_selection import StratifiedKFold
-
-def Validation(k):
-    return StratifiedKFold(n_splits=k, shuffle=True, random_state=831)
